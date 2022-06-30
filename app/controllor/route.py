d0785cb0d71d11b74551523a44c73585a496f396
Zncl2222 <3002shinning@gmail.com>
2022-06-30 18:25:32 +0800

    fix: delete duplicate function

--- app/controllor/route.py
+++ app/controllor/route.py
@@ -38,12 +38,6 @@ def statistic_count():
     data = database.select_uc_sgsim_distincition()
     return render_template("/statistic/statistic_count.html",data=data)
 
-@app.route("/statistic/date")
-def statistic_count():
-    database = db.database()
-    data = database.select_uc_sgsim_distincition()
-    return render_template("/statistic/statistic_count.html",data=data)
-
 @app.route("/acheivement")
 def acheivement():
     return render_template("acheivement.html")
