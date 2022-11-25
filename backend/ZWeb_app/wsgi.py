da04bc9598e501927720d8cf421eb05f27515bc7
bradley_chen <bradley_chen@wistron.com>
2022-11-25 01:18:49 +0000

    test

--- backend/ZWeb_app/wsgi.py
+++ backend/ZWeb_app/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Todolist_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ZWeb_app.settings')
 
 # application = get_wsgi_application()
 application = Cling(get_wsgi_application())
