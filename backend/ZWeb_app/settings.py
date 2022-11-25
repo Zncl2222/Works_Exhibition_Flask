9cad714d05e30e63491cf8fdcffe5eb825077d8c
bradley_chen <bradley_chen@wistron.com>
2022-11-25 09:09:51 +0000

    test

--- backend/ZWeb_app/settings.py
+++ backend/ZWeb_app/settings.py
@@ -23,7 +23,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS = ['https://p01--punchy-step--9qrp2lm78j85.code.run/']
+CSRF_TRUSTED_ORIGINS = ['https://p01--punchy-step--9qrp2lm78j85.code.run']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
