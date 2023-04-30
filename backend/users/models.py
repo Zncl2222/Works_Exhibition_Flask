a400a1daba5c761b81b1183efab1e84bcbfb6dac
Zncl2222 <3002shinning@gmail.com>
2023-05-01 01:14:18 +0800

    feat(users): add email validation

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
-    pass
+    email_validated = models.BooleanField(default=False)
+
+
+class EmailValidationToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=64, unique=True)
+    created_at = models.DateTimeField(default=timezone.now)
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from .views import RegisterView
+from .views import EmailValidationView, EmailValidationConfirmView, RegisterView
+
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
+    path('email-validation/', EmailValidationView.as_view(), name='email_validation'),
+    path(
+        'email-validation/confirm/<str:token>/',
+        EmailValidationConfirmView.as_view(),
+        name='email_validation_confirm',
+    ),
 ]
