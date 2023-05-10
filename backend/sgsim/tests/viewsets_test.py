e3428f87350acb500f7506aae394c036e6355a2d
Zncl2222 <3002shinning@gmail.com>
2023-05-10 22:33:09 +0800

    test(sgsim): add unittest of sgsim

---
+++ backend/sgsim/tests/viewsets_test.py
@@ -0,0 +1,69 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from users.models import User
+
+
+class ViewSetsTests(APITestCase):
+    def setUp(self):
+        self.customer = User.objects.create(
+            username='test',
+            email='test@zweb.com',
+            is_staff=True,
+            is_active=True,
+        )
+        self.client.force_authenticate(user=self.customer)
+
+    def test_email_failed_sgsim(self):
+        data = {
+            'realizations_number': 10,
+            'cov_model': 'Gaussian',
+            'kernel': 'Python',
+            'bandwidth': 35,
+            'bandwidth_step': 1,
+            'x_size': 150,
+            'y_size': 0,
+            'randomseed': 12151,
+            'krige_range': 17.32,
+            'krige_sill': 1,
+        }
+        resp = self.client.post(
+            '/sgsim/',
+            data,
+        )
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_email_failed_sgsim_list(self):
+        resp = self.client.get(
+            '/sgsimlist/',
+        )
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_sgsim(self):
+        data = {
+            'realizations_number': 10,
+            'cov_model': 'Gaussian',
+            'kernel': 'Python',
+            'bandwidth': 35,
+            'bandwidth_step': 1,
+            'x_size': 150,
+            'y_size': 0,
+            'randomseed': 12151,
+            'krige_range': 17.32,
+            'krige_sill': 1,
+        }
+        self.customer.email_validated = True
+        self.customer.save()
+        resp = self.client.post(
+            '/sgsim/',
+            data,
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+    def test_sgsim_list(self):
+        self.customer.email_validated = True
+        self.customer.save()
+        resp = self.client.get(
+            '/sgsimlist/',
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
